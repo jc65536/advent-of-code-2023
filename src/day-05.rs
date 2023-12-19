bf699c6c797df7f3ec4ce4a30240db2fcd670c2d
Jason Cheng <jasoncheng224@gmail.com>
2023-12-19 15:14:33 -0800

    Day 5 part 2

--- src/day-05.rs
+++ src/day-05.rs
@@ -12,6 +12,37 @@ use nom::{
 #[derive(Debug)]
 struct Map(Vec<(u64, u64, u64)>);
 
+// To avoid overflow panic
+const MAX: u64 = u64::MAX / 2;
+
+fn _merge(
+    v: &[(u64, u64, u64)],
+    w: &[(u64, u64, u64)],
+    mut dest: Vec<(u64, u64, u64)>,
+) -> Vec<(u64, u64, u64)> {
+    if v.is_empty() || w.is_empty() {
+        dest
+    } else {
+        let &(d1, s1, l1) = v.first().unwrap();
+        let &(d2, s2, l2) = w.first().unwrap();
+        if d1 + l1 <= s2 {
+            _merge(&v[1..], w, dest)
+        } else if s2 + l2 <= d1 {
+            _merge(v, &w[1..], dest)
+        } else if v.len() == 1 {
+            let lb = d1.max(s2);
+            let ub = (d1 + l1).min(s2 + l2);
+            let s3 = s1 + lb - d1;
+            let d3 = d2 + lb - s2;
+            let l3 = ub - lb;
+            dest.push((d3, s3, l3));
+            _merge(v, &w[1..], dest)
+        } else {
+            _merge(&v[1..], w, _merge(&v[0..1], w, dest))
+        }
+    }
+}
+
 impl Map {
     fn add(&mut self, dest_start: u64, src_start: u64, range_len: u64) {
         self.0.push((dest_start, src_start, range_len));
@@ -24,6 +55,48 @@ impl Map {
             }
         }
         src
+    }
+
+    fn complete(&mut self) {
+        let v = &mut self.0;
+        v.sort_by(|(_, s1, _), (_, s2, _)| s1.cmp(s2));
+        let mut additional: Vec<(u64, u64, u64)> = Vec::new();
+
+        let first = v.first().unwrap().1;
+        if first > 0 {
+            additional.push((0, 0, first));
+        }
+
+        v.windows(2).for_each(|w| {
+            let ((_, s1, l1), (_, s2, _)) = (w[0], w[1]);
+            if s1 + l1 < s2 {
+                additional.push((s1 + l1, s1 + l1, s2 - (s1 + l1)))
+            }
+        });
+
+        let last_entry = v.last().unwrap();
+        let last = last_entry.1 + last_entry.2;
+        if last <= MAX {
+            additional.push((last, last, MAX - last + 1))
+        }
+
+        v.append(&mut additional);
+    }
+
+    fn merge(&mut self, other: &mut Map) -> Map {
+        let v = &mut self.0;
+        v.sort_by(|(d1, _, _), (d2, _, _)| d1.cmp(d2));
+
+        let w = &mut other.0;
+        w.sort_by(|(_, s1, _), (_, s2, _)| s1.cmp(s2));
+
+        Map(_merge(v, w, Vec::new()))
+    }
+
+    fn print(&self) {
+        for &(a, b, c) in &self.0 {
+            println!("{a:20}{b:20}{c:20}");
+        }
     }
 }
 
@@ -63,6 +136,26 @@ impl Almanac {
         }
 
         min_location
+    }
+
+    fn complete(&mut self) {
+        self.seed_soil_map.complete();
+        self.soil_fertilizer_map.complete();
+        self.fertilizer_water_map.complete();
+        self.water_light_map.complete();
+        self.light_temperature_map.complete();
+        self.temperature_humidity_map.complete();
+        self.humidity_location_map.complete();
+    }
+
+    fn merge_all(&mut self) -> Map {
+        self.seed_soil_map
+            .merge(&mut self.soil_fertilizer_map)
+            .merge(&mut self.fertilizer_water_map)
+            .merge(&mut self.water_light_map)
+            .merge(&mut self.light_temperature_map)
+            .merge(&mut self.temperature_humidity_map)
+            .merge(&mut self.humidity_location_map)
     }
 }
 
@@ -146,10 +239,33 @@ fn parse_almanac<'a>(
 
 fn part1() -> u64 {
     let (_, almanac) = unwrap(parse_almanac(Box::new(stdin().lines().map(|x| x.unwrap()))));
-    println!("{almanac:?}");
     almanac.get_lowest_loc()
 }
 
+fn part2() -> u64 {
+    let (_, mut almanac) = unwrap(parse_almanac(Box::new(stdin().lines().map(|x| x.unwrap()))));
+
+    almanac.complete();
+
+    // let mut d = almanac.seed_soil_map.merge(&mut almanac.soil_fertilizer_map);
+    // println!("seed soil:");
+    // almanac.seed_soil_map.print();
+    // println!("soil fertilizer:");
+    // almanac.soil_fertilizer_map.print();
+    // println!("merged:");
+    // d.0.sort_by(|(_, s1, _), (_, s2, _)| s1.cmp(s2));
+    // d.print();
+
+    let mut seeds = Map(almanac.seeds.chunks(2).map(|ch| (ch[0], ch[0], ch[1])).collect());
+
+    let mut m = seeds.merge(&mut almanac.merge_all());
+    m.0.sort_by(|(d1, _, _), (d2, _, _)| d1.cmp(d2));
+
+    let first = m.0.first().unwrap();
+
+    first.0
+}
+
 fn main() {
-    println!("{}", part1());
+    println!("{}", part2());
 }
